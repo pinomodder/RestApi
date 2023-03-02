3d61863d357c57389dce80d4d850215fffe9e4e2
pinomodder <120091085+pinomodder@users.noreply.github.com>
2023-03-02 14:56:02 +0700

    Add files via upload

---
+++ views/uploader/upload.js
@@ -0,0 +1,24 @@
+const url = 'process.php';
+const form = document.querySelector('form');
+
+form.addEventListener('submit', e => {
+    e.preventDefault();
+
+    const files = document.querySelector('[type=file]').files;
+    const formData = new FormData();
+
+    for (let i = 0; i < files.length; i++) {
+        let file = files[i];
+
+        formData.append('files[]', file);
+    }
+
+    fetch(url, {
+        method: 'POST',
+        body: formData
+    }).then(response => {
+        return response.text();
+    }).then(data => {
+        console.log(data);
+    });
+});
