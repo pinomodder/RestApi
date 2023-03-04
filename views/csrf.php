b0c2483d067c83d66bb63f7ae2e7afc7bf38e478
PINO MODDER <120091085+pinomodder@users.noreply.github.com>
2023-03-05 02:39:36 +0700

    Add files via upload
    Signed-off-by: PINO MODDER <120091085+pinomodder@users.noreply.github.com>

---
+++ views/csrf.php
@@ -0,0 +1,120 @@
+<html>
+<head>
+<meta name="googlebot" content="all,index,follow">
+<meta name="robots" content="index, follow">
+<title>CSRF Online | Pino </title>
+<link rel="shortcut icon" href="https://cdn.jsdelivr.net/gh/pinomodder/Animex@main/static/img/cover.jpg">
+<link href='https://fonts.googleapis.com/css?family=Inconsolata' rel='stylesheet' type='text/css'>
+</head>
+
+<style>
+body {
+ padding: 0;
+ margin: 0;
+ background-image: url('');
+ background-repeat:no-repeat;
+ background-position:center;
+ background-size: 100% 100%;
+}
+#preloader {
+ position:fixed;
+ top:0;
+ left:0;
+ right:0;
+ bottom:0;
+ background-color:#fff;
+ z-index:99; 
+}
+
+#loading {
+ width:200px;
+ height:200px;
+ position:absolute;
+ top: 40%;
+ left: 50%;
+ background-image:url(https://lh3.googleusercontent.com/proxy/hu1zs9nOfikMda14mwaf4hfF-lgRxsr8FqPUXlgHEVtGsrORG6z7qg4j_kDLRyoS1KJieoWOevo406NOtkUVndVa3CLFo_xLwq1nrDE=w495-h297-nc); 
+ background-repeat:no-repeat;
+ background-position:center;
+ margin:-100px 0 0 -100px; 
+}
+.content {
+ width: 500px;
+ margin: 10px auto;
+}
+.content img {
+ width: 500px;
+ margin: 100px auto;
+}
+.login-input {
+  color: white;
+  -webkit-font-smoothing: antialiased;
+  background-color: rgba(0, 255, 252, 0.05);
+  background-image: -webkit-linear-gradient(rgba(0, 192, 255, 0.05), rgba(0, 192, 255, 0), rgba(0, 192, 255, 0.05));
+  background-image: -moz-linear-gradient(rgba(0, 192, 255, 0.05), rgba(0, 192, 255, 0), rgba(0, 192, 255, 0.05));
+  background-image: -o-linear-gradient(rgba(0, 192, 255, 0.05), rgba(0, 192, 255, 0), rgba(0, 192, 255, 0.05));
+  border: 1px solid #2ACBF3;
+  -moz-border-radius: 5px;
+  -webkit-border-radius: 5px;
+  border-radius: 5px;
+  -webkit-box-shadow: 0 0 16px rgba(0, 192, 255, 0.3), inset 0 0 20px rgba(0, 192, 255, 0.3);
+  -moz-box-shadow: 0 0 16px rgba(0, 192, 255, 0.3), inset 0 0 20px rgba(0, 192, 255, 0.3);
+  box-shadow: 0 0 16px rgba(0, 192, 255, 0.3), inset 0 0 20px rgba(0, 192, 255, 0.3);
+  -webkit-transition: border 200ms ease, -webkit-transform 0s ease;
+  -moz-transition: border 200ms ease, -moz-transform 0s ease;
+  -o-transition: border 200ms ease, -o-transform 0s ease;
+  outline: none;
+  text-align: left;
+  font-size: 15px;
+  color: #fff;
+  padding: 10px;
+  width: 300px;
+  font-family: Menlo, Consolas, Monaco, "Lucida Console", monospace;
+}
+input[type=file] {
+	width: 250px;
+	height: 33px;
+	color: red;
+	background: #000000;
+	border: 1px dotted #2ACBF3;
+	padding: 5px;
+	margin-left: 20px;
+	text-align: center;
+}
+input[type=text] {
+	width: 251px;
+	height: 33px;
+	color: red;
+	background: #000000;
+	border: 1px dotted #2ACBF3;
+	padding: 5px;
+	margin-left: 20px;
+	text-align: center;
+}
+header {
+	color: #2ACBF3;
+	margin: 10px auto;
+}
+</style>
+<body bgcolor=black>
+<center>
+<header>
+	<pre>
+C S R F  O N L I N E
+	</pre>
+</header>
+	<center>
+<form method="post">
+<font face="Inconsolata" color="white"> URL TARGET : </font> <font color="black"> -------------- </font> <input type="text" name="url" placeholder="Target Lu Cok :)" style="margin: 5px auto; padding-left: 5px;" required>
+<br><br><font face="Inconsolata" color="white"> POST File : </font><font color="black">-</font>  <input type="text" name="pf" size="50" height="10" placeholder="Filedata / files[] / qqfile / userfile / dll" required>
+<br><br><input type = "submit" name = "submit" value = "Skakmat Target" class="login-input"style='width: 130px; height: 40px;'> </ form >
+</form>
+</form>
+<script src="http://code.jquery.com/jquery-2.1.1.min.js"></script>
+    <script type="text/javascript">
+  $(window).load(function() {
+   $("#content").fadeOut();
+   $("#preloader").delay(10).fadeOut("slow");
+  })
+    </script>
+    </body>
+</html>
